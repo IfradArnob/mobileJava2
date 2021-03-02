5a1fa9f7621ab8e387b79b7b2b5a439ee605a94f
Ifrad Arnob <arnobifrad2@gmail.com>
2021-03-02 17:26:49 +0600

    Commit juan

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Mobile m1= new Mobile("Arnob", "01231313412","Android",200,true);
+        m1.showinfo();
+        Mobile m2= new Mobile("Arnob2", "01696969420","Android",10000,false);
+        m2.showinfo();
+
+    }
+}
